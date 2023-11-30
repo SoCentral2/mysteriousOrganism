439e96fc6a18d9f3d3731b73ef0eb99500cfefc2
Paul Rathbone <socentral2@hotmail.co.uk>
2023-11-30 11:57:38 +0000

    completed project with debug output and comments

--- mysteriousOrganism.js
+++ mysteriousOrganism.js
@@ -1,4 +1,5 @@
 // Returns a random DNA base
+// Most header documents generatied by Mintlify.
 const returnRandBase = () => {
     const dnaBases = ['A', 'T', 'C', 'G']
     return dnaBases[Math.floor(Math.random() * 4)] 
@@ -15,6 +16,16 @@ const returnRandBase = () => {
   
   
   
+ /**
+  * The `pAequorFactory` function creates objects representing specimens of a fictional organism,
+  * allowing for mutation, DNA comparison, and survival probability calculation.
+  * @param specimenNum - The specimenNum parameter is the number assigned to a specific specimen of
+  * pAequor. It is used to identify and differentiate between different specimens.
+  * @param bases - The `bases` parameter represents an array of DNA bases. Each base can be one of four
+  * options: 'A', 'T', 'C', or 'G'.
+  * @returns The `pAequorFactory` function returns an object with properties and methods related to a
+  * specimen of pAequor.
+  */
   let pAequorFactory = function (specimenNum, bases) {
     return {
 
@@ -22,6 +33,8 @@ const returnRandBase = () => {
       bases : bases,
 
 
+     /* The `mutate()` function is a method of the `pAequorFactory` object. It is used to randomly
+     change one base in the DNA sequence of the pAequor specimen. */
       mutate() {
         let basePositionToChange = 0;
         let baseToChange = '';
@@ -36,6 +49,9 @@ const returnRandBase = () => {
         return returnBases;
       },
       
+     /* The `compareDNA` method is used to compare the DNA of two pAequor specimens. It takes another
+     pAequor object as a parameter (`_pAequor`) and calculates the percentage of DNA bases that are
+     the same between the two specimens. */
       compareDNA(_pAequor) {
         let basesInCommon = 0;
         console.log("_pAequor.bases.length = " + _pAequor.bases.length);
@@ -49,6 +65,13 @@ const returnRandBase = () => {
         console.log(`Specimin #1 and specimin #2 have ${basesInCommon}% DNA in common.`);
       },
 
+      
+       /* The function "willLikelySurvive" checks if the percentage of 'C' and 'G' bases in a given
+       * sequence is at least 60%.
+       * @returns The function will return a boolean value. If the percentage of 'C' and 'G' bases in
+       * the given array is greater than or equal to 60%, it will return true. Otherwise, it will
+       * return false.
+       */
       willLikelySurvive() {
        let cAndgCounter = 0;
         for (i = 0; i < bases.length-1; i++) {
@@ -87,6 +110,16 @@ console.log(pAequor1.willLikelySurvive());
    while (!getThirtyViableBases(loopCount = i));
     }
 
+
+  /**
+   * The function `getThirtyViableBases` generates and checks 30 DNA strands for viability, and logs
+   * the results.
+   * @param _loopCount - The _loopCount parameter is used to keep track of the number of times the
+   * function has been called or the current iteration of the loop. It is passed as an argument to the
+   * function.
+   * @returns a boolean value. If the condition `pAequor.willLikelySurvive()` is true, it returns
+   * `true`. Otherwise, it returns `false`.
+   */
   function getThirtyViableBases (_loopCount) {
     let pAequor = null;
     pAequor = pAequorFactory(1, mockUpStrand());
